bb3bf30f2a3162ad438d111b44da146a10949284
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2025-12-19 19:39:33 +0530

    Time: 3 ms (85.09%) | Memory: 18.4 MB (24.37%) - LeetSync

--- 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.py
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.py
@@ -1,9 +1,3 @@
 class Solution:
     def minLengthAfterRemovals(self, s: str) -> int:
-        stack=[]
-        for i in s:
-            if stack and stack[-1]!=i:
-                stack.pop()
-            else:
-                stack.append(i)
-        return len(stack)
+        return abs(s.count('a')-s.count('b'))
