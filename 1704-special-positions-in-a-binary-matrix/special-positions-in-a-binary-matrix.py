069be5613774b09cd6130879ed663458d50a999a
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-03-04 15:57:29 +0530

    Time: 11 ms (22.65%) | Memory: 20.6 MB (5.18%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -3,7 +3,6 @@ class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
         #take temp for getting postion of 1 in the matrix
         #row_count and col_count is used to count the frequency of how many times 1 appeared in row and col
-        #unique_row,unique_col used to store the unique occurence of position 1 where others are zeroes
         ans=0
         temp=set()
         row_count,col_count=Counter(),Counter()
@@ -15,17 +14,8 @@ class Solution:
                     row_count[i]+=1
                     col_count[j]+=1
 
-        #below is the process of getting unique row and column positions where it is occured only 1 time
-        for val,freq in row_count.items():
-            if freq==1:
-                unique_row.append(val)
-        for val,freq in col_count.items():
-            if freq==1:
-                unique_col.append(val)
-
         #counting the number of special positions in the matrix
-        for i in unique_row:
-            for j in unique_col:
-                if (i,j) in temp:
+        for x in temp:
+            if row_count[x[0]]==1 and col_count[x[1]]==1:
                     ans+=1
         return ans
