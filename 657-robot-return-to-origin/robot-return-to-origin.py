39700545bb50a6b37561876e862cd0d610ebabd7
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-04-05 10:50:28 +0530

    Time: 15 ms (60.08%) | Memory: 19.4 MB (10.66%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -1,5 +1,16 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        if moves.count('U')==moves.count('D') and moves.count('L')==moves.count('R'):
+        x=0
+        y=0
+        for i in moves:
+            if i=='U':
+                x+=1
+            elif i=='D':
+                x-=1
+            elif i=='R':
+                y+=1
+            elif i=='L':
+                y-=1
+        if x==0 and y==0:
             return True
         return False
