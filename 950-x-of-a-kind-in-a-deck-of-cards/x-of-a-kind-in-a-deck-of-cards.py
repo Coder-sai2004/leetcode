dea136299cdf18909dd9d28d2189cbb355ba438b
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-05-01 23:06:36 +0530

    Time: 2 ms (82.03%) | Memory: 19.7 MB (72.93%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,6 @@
+from collections import Counter
+from math import gcd
+from functools import reduce
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        return reduce(gcd,Counter(deck).values())>=2
