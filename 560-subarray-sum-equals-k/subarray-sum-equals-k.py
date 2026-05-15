3f70b408f25e4958f31980571900a18c1b8e4d32
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-05-15 19:54:30 +0530

    Time: 54 ms (6.32%) | Memory: 22.4 MB (16.45%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        #taking prefix sum of nums and s for frequency to calculate the subarrays
         pre=[nums[0]]
         s={}
         c=0
-        
+        #calculating the prefix sum of nums
         for i in range(1,len(nums)):
             pre.append(pre[i-1]+nums[i])
         c+=pre.count(k)
 
+        #here we calculate the difference between current element and k value and check whether it 
+        #exists in the hash table and also consider how many times the difference is there to 
+        #calculate correct count of subarrays
         for i in range(len(pre)):
             x=pre[i]-k
 
@@ -18,4 +22,5 @@ class Solution:
                 c+=s[x]
 
             s[pre[i]]=s.get(pre[i],0)+1
+            
         return c
