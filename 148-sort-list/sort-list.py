4f8e1076ea8b7dfae963d20b7411d364c73e90ad
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-06-20 11:23:49 +0530

    Time: 179 ms (50.93%) | Memory: 40.9 MB (15.12%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -4,20 +4,47 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        res=[]
-        while head:
-            res.append(head.val)
-            head=head.next
-        res.sort()
-        new=None
-        tail=None
-        for i in res:
-            node=ListNode(i)
-            if new is None:
-                new=node
-                tail=node
+    def middle(self,head):
+        slow=head
+        fast=head.next
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+
+
+    def merge(self,left,right):
+        dummy=ListNode(0)
+        tail=dummy
+
+        while left and right:
+            if left.val<=right.val:
+                tail.next=left
+                left=left.next
             else:
-                tail.next=node
-                tail=tail.next
-        return new
+                tail.next=right
+                right=right.next
+
+            tail=tail.next
+
+        if left:
+            tail.next=left
+
+        if right:
+            tail.next=right
+
+        return dummy.next
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+
+        m=self.middle(head)
+        
+        right_half=m.next
+        m.next=None
+
+        left=self.sortList(head)
+        right=self.sortList(right_half)
+
+        return self.merge(left,right)
