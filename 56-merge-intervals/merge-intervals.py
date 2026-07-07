4c6f18677401656ad8887301ebb522382c25edb0
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-07-07 19:34:15 +0530

    Time: 5 ms (80.39%) | Memory: 23.4 MB (7.23%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,32 +1,37 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals)<=1:
+        if len(intervals) <= 1:
             return intervals
-        intervals.sort(key=lambda x: x[0])
-        mi=intervals[0][0]
-        mx=intervals[0][1]
-        n=len(intervals)-1
-        res=[]
-        for i in range(1,len(intervals)):
-            cur_mi=intervals[i][0]
-            cur_mx=intervals[i][1]
 
-            #if the current intervals is in between the previous intervals
-            if cur_mi<mx and cur_mx<mx:
-                if i==n:
-                    res.append([mi,mx])
+        #sorting the intervals based on the first value of the interval
+        intervals.sort(key=lambda x: x[0])
+
+        mi = intervals[0][0]
+        mx = intervals[0][1]
+        n = len(intervals) - 1
+        res = []
+
+        for i in range(1, len(intervals)):
+            cur_mi = intervals[i][0]
+            cur_mx = intervals[i][1]
+
+            # if the current intervals is in between the previous intervals
+            if cur_mi < mx and cur_mx < mx:
+                if i == n:
+                    res.append([mi, mx])
                 continue
 
-            #if the current and previous intervals overlap
-            elif cur_mi<=mx:
-                mx=cur_mx
+            # if the current and previous intervals overlap
+            elif cur_mi <= mx:
+                mx = cur_mx
 
-            #if the current and previous intervals not related
-            elif mx<cur_mi:
-                res.append([mi,mx])
-                mi=cur_mi
-                mx=cur_mx
-                
-            if i==n:
-                    res.append([mi,mx])
+            # if the current and previous intervals not related
+            elif mx < cur_mi:
+                res.append([mi, mx])
+                mi = cur_mi
+                mx = cur_mx
+
+            if i == n:
+                res.append([mi, mx])
+
         return res
