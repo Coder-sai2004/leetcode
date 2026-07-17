722935f99f09f913d089bff517d43482daee09cc
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-07-17 20:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (57.25%) - LeetSync

---
+++ 57-insert-interval/insert-interval.py
@@ -0,0 +1,38 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        #appending the new interval into the list
+        intervals.append(newInterval)
+        if len(intervals) <= 1:
+            return intervals
+
+        #sorting the intervals based on the first value of the interval
+        intervals.sort(key=lambda x: x[0])
+
+        mi = intervals[0][0]
+        mx = intervals[0][1]
+        n = len(intervals) - 1
+        res = []
+
+        for i in range(1, len(intervals)):
+            cur_mi = intervals[i][0]
+            cur_mx = intervals[i][1]
+
+            # if the current intervals is in between the previous intervals
+            if cur_mi < mx and cur_mx < mx:
+                if i == n:
+                    res.append([mi, mx])
+                continue
+
+            # if the current and previous intervals overlap
+            elif cur_mi <= mx:
+                mx = cur_mx
+
+            # if the current and previous intervals not related
+            elif mx < cur_mi:
+                res.append([mi, mx])
+                mi = cur_mi
+                mx = cur_mx
+
+            if i == n:
+                res.append([mi, mx])
+        return res
