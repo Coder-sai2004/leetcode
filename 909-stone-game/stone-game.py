e91c48a801ea9e725a6015ca5572b50bf8fab86f
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-08-02 11:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (75.06%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,3 +1,38 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        # left and right are the starting and ending indices.
+        left = 0
+        right = len(piles) - 1
+
+        # turn indicates whose turn it is.
+        turn = 0
+
+        alice = 0
+        bob = 0
+
+        while left < right:
+            # We consider the first and last piles along with their adjacent
+            # piles to decide which pile gives the better next move.
+            left_stone = piles[left]
+            next_left = piles[left + 1]
+            right_stone = piles[right]
+            next_right = piles[right - 1]
+
+            if turn % 2 == 0:
+                if next_left < next_right:
+                    alice += left_stone
+                else:
+                    alice += right_stone
+            else:
+                if next_left < next_right:
+                    alice += left_stone
+                else:
+                    alice += right_stone
+
+            left += 1
+            right -= 1
+            turn += 1
+
+        if alice > bob:
+            return True
+        return False
