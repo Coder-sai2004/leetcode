541ad42b129e1d0b9a73ea4e673362804ef77778
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-08-06 11:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 26.5 MB (71.09%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,8 +4,6 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k=k%len(nums)
-        while k>0:
-            v=nums.pop()
-            nums.insert(0,v)
-            k-=1
-        return nums
+        nums.reverse()
+        nums[:k]=reversed(nums[:k])
+        nums[k:]=reversed(nums[k:])
