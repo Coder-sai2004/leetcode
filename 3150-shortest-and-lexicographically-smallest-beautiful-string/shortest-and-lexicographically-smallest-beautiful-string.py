0c729b658881ba4fcde97a9c0bd0a2a68927cc51
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-08-26 11:11:26 +0530

    Time: 3 ms (53.15%) | Memory: 19.3 MB (30.63%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,29 +1,33 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        s+='0'
-        i=0
-        j=0
-        c=0
-        res=float('inf')
-        ans=''
-        while j<len(s):
-            while c==k:
-                
-                if (j-i)==res:
-                    res=j-i
-                    if s[i:j]<ans:
-                        ans=s[i:j]
+        # Add extra 0 for the end case
+        s += '0'
+        left = 0
+        right = 0
+        count = 0
+        length = float('inf')
+        ans = ''
+        
+        while right < len(s):
+            while count == k:
+                # If lengths match, choose the smaller substring
+                if (right - left) == length:
+                    if s[left:right] < ans:
+                        ans = s[left:right]
 
-                elif (j-i)<res:
-                    res=j-i
-                    ans=s[i:j]
+                # If shorter, assign immediately
+                elif (right - left) < length:
+                    length = right - left
+                    ans = s[left:right]
 
-                if s[i]=='1':
-                    c-=1
-                i+=1
+                # Shrink window from the left
+                if s[left] == '1':
+                    count -= 1
+                left += 1
 
-            if s[j]=='1':
-                c+=1
-            j+=1
-    
+            # Count 1's in the window
+            if s[right] == '1':
+                count += 1
+            right += 1
+        
         return ans
