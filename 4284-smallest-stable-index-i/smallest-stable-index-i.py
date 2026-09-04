31be36dac312168e4c7a6211acd90f265441504e
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-09-04 07:53:20 +0530

    Time: 3 ms (77.34%) | Memory: 19.3 MB (71.43%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -0,0 +1,24 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        #initialize mx and mi to store max and min values at the current index
+        mx = []
+        mi = []
+        n = len(nums)
+        
+        #l is used to store max value while r is used to store min value
+        l = 0
+        r = float('inf')
+        for i in range(n):
+            l = max(l,nums[i])
+            r = min(r,nums[n-i-1])
+            mx.append(l)
+            mi.append(r)
+        
+        #reversing the mi because we are appending from front,but we want min values from end
+        mi = mi[::-1]
+
+        #calculation of smallest stable index where the instability score becomes <= k
+        for i in range(n):
+            if mx[i] - mi[i] <= k:
+                return i
+        return -1
