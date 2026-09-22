cba6f5a3137ea3dd817d5594ba72898ec9a9faa4
Tanuku_Ram_Sai <dcmeramsai003@gmail.com>
2026-09-22 11:53:11 +0530

    Time: 77 ms (50.85%) | Memory: 36.1 MB (76.46%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
@@ -1,20 +1,16 @@
 class Solution:
-    def maxDistance(self, nums1: list[int], nums2: list[int]) -> int:
-        ans = 0
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
         left = 0
         right = 0
-
-        while left < len(nums1) and right < len(nums2):
-            if nums1[left] <= nums2[right] and left <= right:
-
-                ans = max(ans, right - left)
-
-                right += 1
-
-            elif nums1[left] > nums2[right]:
-                left += 1
-                
+        ans = 0
+        while left<len(nums1) and right<len(nums2):
+            #adding the maximum distance when left index and value less than or equal to right index and value.
+            if nums1[left]<=nums2[right] and left<=right:
+                ans=max(ans,right-left)
+                right+=1
+            #when the left index or value greater than right index or value
+            else:
+                left+=1
                 if left > right:
                     right = left
-        
         return ans
